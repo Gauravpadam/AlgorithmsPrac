7528d8b976f0727149983704b2d7f5285eaa07a3
Gaurav <gauravpadam2@gmail.com>
2023-04-25 07:27:49 +0530

    number play

---
+++ Linear Search/evendigitcount.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        counteven = 0
+        countdigit = 0
+
+        #Logic: bs question
+                #Iterate over all numbers
+                #Set a number into temp and keep dividing it till it becomes 0
+                #Meanwhile this happens, digitcount+1 on each division
+                #Now modulo digitcount by 2, if 0, counteven + 1
+                #Reset digitcount on each iteration to 0
+
+                #(Another approach would be str conversion and taking length)
+
+        for i in range(len(nums)):
+            temp = nums[i] # Just so to not mess up the original array by dividing
+            while temp>0:
+                temp = temp//10 # Dividing to reduce digits by one position at a time
+                countdigit+=1 # Increasing digit count
+            if countdigit % 2 == 0: # even check
+                counteven+=1
+            countdigit = 0 # Resetting digitcount
+
+        return counteven
